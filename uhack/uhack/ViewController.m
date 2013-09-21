22139651a708ae7936af1f523f3a5a6d20af8a11
Anders Hassis <anders.hassis@gmail.com>
2013-09-21 15:39:06 +0200

    Removed alert box

--- uhack/uhack/ViewController.m
+++ uhack/uhack/ViewController.m
@@ -212,21 +212,7 @@
                                 [self.searchResults reloadData];
                             }
                             failure:^(RKObjectRequestOperation *operation, NSError *error) {
-                                FUIAlertView *alertView = [[FUIAlertView alloc] initWithTitle:@"Fel"
-                                                                                      message:[error localizedDescription]
-                                                                                     delegate:nil cancelButtonTitle:nil
-                                                                            otherButtonTitles:@"Ok", nil];
-                                alertView.titleLabel.textColor = [UIColor cloudsColor];
-                                alertView.titleLabel.font = [UIFont boldFlatFontOfSize:16];
-                                alertView.messageLabel.textColor = [UIColor cloudsColor];
-                                alertView.messageLabel.font = [UIFont flatFontOfSize:14];
-                                alertView.backgroundOverlay.backgroundColor = [[UIColor cloudsColor] colorWithAlphaComponent:0.8];
-                                alertView.alertContainer.backgroundColor = [UIColor midnightBlueColor];
-                                alertView.defaultButtonColor = [UIColor cloudsColor];
-                                alertView.defaultButtonShadowColor = [UIColor asbestosColor];
-                                alertView.defaultButtonFont = [UIFont boldFlatFontOfSize:16];
-                                alertView.defaultButtonTitleColor = [UIColor asbestosColor];
-                                [alertView show];
+                                debugLog(@"Error: %@", [error localizedDescription]);
                             }];
     return YES;
 }
